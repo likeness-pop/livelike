13f5f76d8316ce9f6ee31350d9842550aefba3e6
Joe <tuccillojv@ornl.gov>
2025-05-07 12:32:10 -0400

    add attribution docstrings

--- livelike/attribution.py
+++ livelike/attribution.py
@@ -3,6 +3,24 @@ import pandas as pd
 import re
 
 def columns_to_labels(df, col_prefix, scrub=None):
+    """
+    Converts dummy columns to labels. 
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Data frame containing dummy columns.
+    col_prefix : str
+        A regular expression for filtering the columns to 
+        use for labeling.
+    scrub : str
+        A regular expression for label text to exclude.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Converted labels.
+    """
     df = df.loc[:,df.columns.str.contains(col_prefix)]
 
     df_ = pd.DataFrame()
@@ -18,6 +36,19 @@ def columns_to_labels(df, col_prefix, scrub=None):
 ## Labeling Functions ##
 
 def class_of_worker(est_person):
+    """
+    Produces person-level labels for class of worker. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Class of worker labels.
+    """
     prefix = "^sexcw_male_|^sexcw_female_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "class_of_worker"
@@ -26,6 +57,19 @@ def class_of_worker(est_person):
 
 
 def commute_time_m(est_person):
+    """
+    Produces person-level labels for commute time in minutes. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Commute time labels.
+    """
     prefix = "^cmt_mins_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "commute_time_m"
@@ -34,6 +78,19 @@ def commute_time_m(est_person):
 
 
 def commute_mode(est_person):
+    """
+    Produces person-level labels for commute mode. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Commute mode labels.
+    """
     prefix = "^travel_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "commute_mode"
@@ -42,6 +99,19 @@ def commute_mode(est_person):
 
 
 def commute_drive_type(est_person):
+    """
+    Produces person-level labels for commute drive type. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Commute drive type labels.
+    """
     prefix = "^veh_occ_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "commute_drive_type"
@@ -50,6 +120,19 @@ def commute_drive_type(est_person):
 
 
 def employment_status(est_person):
+    """
+    Produces person-level labels for employment status. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Employment status labels.
+    """
     prefix = "emp_stat_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "employment_status"
@@ -58,6 +141,19 @@ def employment_status(est_person):
 
 
 def grade(est_person):
+    """
+    Produces person-level labels for school grade lavel. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Grade level labels.
+    """
     prefix = "^grade_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "grade"
@@ -66,6 +162,19 @@ def grade(est_person):
 
 
 def hours_worked(est_person):
+    """
+    Produces person-level labels for hours worked. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Hours worked labels.
+    """
     prefix = "^male_hours_|^female_hours_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "hours_worked"
@@ -74,6 +183,19 @@ def hours_worked(est_person):
 
 
 def household_size(est_household):
+    """
+    Produces household-level labels for household size. 
+
+    Parameters
+    ----------
+    est_household: pandas.DataFrame
+        Household-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Household size labels.
+    """
     prefix = "^hht_fam_|^hht_nonfam_"
     scrub = prefix + "|_hhsize_"
     labels = columns_to_labels(est_household, col_prefix=prefix, scrub=scrub)
@@ -83,6 +205,19 @@ def household_size(est_household):
 
 
 def household_type(est_household):
+    """
+    Produces household-level labels for household type. 
+
+    Parameters
+    ----------
+    est_household: pandas.DataFrame
+        Household-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Household type labels.
+    """
     prefix = "^hht_"
     scrub = "hht_|_hhsize_|_2p|_3p|_4p|_5p|_6p|_7pm|"
     labels = columns_to_labels(est_household, col_prefix=prefix, scrub=scrub)
@@ -92,6 +227,19 @@ def household_type(est_household):
 
 
 def industry(est_person):
+    """
+    Produces person-level labels for NAICS industry. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        NAICS industry time labels.
+    """
     prefix = "^sexnaics_female_|^sexnaics_male_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "industry"
@@ -100,6 +248,20 @@ def industry(est_person):
 
 
 def occupation(est_person):
+    """
+    Produces person-level labels for occupation 
+    based on Standard Occupation Classification. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Occupation labels.
+    """
     prefix = "^sexocc_female_|^sexocc_male_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "occupation"
@@ -108,6 +270,19 @@ def occupation(est_person):
 
 
 def school_type(est_person):
+    """
+    Produces person-level labels for school type in minutes. 
+
+    Parameters
+    ----------
+    est_person: pandas.DataFrame
+        Person-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+       School type labels.
+    """
     prefix = "^sch_female_|^sch_male_"
     scrub = (
         "sch_|female_|male_|pre_|kind_|01.04_|05.08_|09.12_"
@@ -120,6 +295,19 @@ def school_type(est_person):
 
 
 def tenure(est_household):
+    """
+    Produces household-level labels for tenure. 
+
+    Parameters
+    ----------
+    est_household: pandas.DataFrame
+        Household-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Household size labels.
+    """
     prefix="^txv_"
     scrub = "txv_|_01_|_02_|_03_|_04_|_GE05_|vehicle"
     labels = columns_to_labels(est_household, col_prefix=prefix, scrub=scrub)
@@ -129,9 +317,22 @@ def tenure(est_household):
 
 
 def vehicles_available(est_household):
+    """
+    Produces household-level labels for number of vehicles available. 
+
+    Parameters
+    ----------
+    est_household: pandas.DataFrame
+        Household-level PUMS constraints.
+
+    Returns
+    -------
+    labels : pandas.Series
+        Vehicles available labels.
+    """
     prefix = "^txv_own_|^txv_rent_"
     scrub = prefix + "|_vehicle"
-    labels = columns_to_labels(est_household, col_prefix=prefix, scrub=prefix)
+    labels = columns_to_labels(est_household, col_prefix=prefix, scrub=scrub)
     labels = labels.replace({"no" : "none"})
     labels.name = "vehicles_available"
 
