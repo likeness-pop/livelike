a78d3367b5ccb45c5b81cb3ae02a7a6f4e6ad4f8
Joe <tuccillojv@ornl.gov>
2025-05-07 12:09:50 -0400

    alphabetize attribution labeling functions

--- livelike/attribution.py
+++ livelike/attribution.py
@@ -15,63 +15,12 @@ def columns_to_labels(df, col_prefix, scrub=None):
 
     return labels
 
-
-def hours_worked(est_person):
-    prefix = "^male_hours_|^female_hours_"
-    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
-    labels.name = "hours_worked"
-
-    return labels
-
-
-def employment_status(est_person):
-    prefix = "emp_stat_"
-    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
-    labels.name = "employment_status"
-
-    return labels    
-
+## Labeling Functions ##
 
 def class_of_worker(est_person):
     prefix = "^sexcw_male_|^sexcw_female_"
     labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
     labels.name = "class_of_worker"
-
-    return labels
-
-
-def occupation(est_person):
-    prefix = "^sexocc_female_|^sexocc_male_"
-    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
-    labels.name = "occupation"
-
-    return labels
-
-
-def industry(est_person):
-    prefix = "^sexnaics_female_|^sexnaics_male_"
-    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
-    labels.name = "industry"
-
-    return labels
-
-
-def school_type(est_person):
-    prefix = "^sch_female_|^sch_male_"
-    scrub = (
-        "sch_|female_|male_|pre_|kind_|01.04_|05.08_|09.12_"
-        "undergrad_|grad.prof_"
-    )
-    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=scrub)
-    labels.name = "school_type"
-
-    return labels
-
-
-def grade(est_person):
-    prefix = "^grade_"
-    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
-    labels.name = "grade"
 
     return labels
 
@@ -100,6 +49,76 @@ def commute_drive_type(est_person):
     return labels
 
 
+def employment_status(est_person):
+    prefix = "emp_stat_"
+    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
+    labels.name = "employment_status"
+
+    return labels
+
+
+def grade(est_person):
+    prefix = "^grade_"
+    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
+    labels.name = "grade"
+
+    return labels   
+
+
+def hours_worked(est_person):
+    prefix = "^male_hours_|^female_hours_"
+    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
+    labels.name = "hours_worked"
+
+    return labels
+
+
+def household_size(est_household):
+    prefix = "^hht_fam_|^hht_nonfam_"
+    scrub = prefix + "|_hhsize_"
+    labels = columns_to_labels(est_household, col_prefix=prefix, scrub=scrub)
+    labels.name = "household_size"
+
+    return labels
+
+
+def household_type(est_household):
+    prefix = "^hht_"
+    scrub = "hht_|_hhsize_|_2p|_3p|_4p|_5p|_6p|_7pm|"
+    labels = columns_to_labels(est_household, col_prefix=prefix, scrub=scrub)
+    labels.name = "household_type",
+
+    return labels
+
+
+def industry(est_person):
+    prefix = "^sexnaics_female_|^sexnaics_male_"
+    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
+    labels.name = "industry"
+
+    return labels
+
+
+def occupation(est_person):
+    prefix = "^sexocc_female_|^sexocc_male_"
+    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=prefix)
+    labels.name = "occupation"
+
+    return labels
+
+
+def school_type(est_person):
+    prefix = "^sch_female_|^sch_male_"
+    scrub = (
+        "sch_|female_|male_|pre_|kind_|01.04_|05.08_|09.12_"
+        "undergrad_|grad.prof_"
+    )
+    labels = columns_to_labels(est_person, col_prefix=prefix, scrub=scrub)
+    labels.name = "school_type"
+
+    return labels
+
+
 def tenure(est_household):
     prefix="^txv_"
     scrub = "txv_|_01_|_02_|_03_|_04_|_GE05_|vehicle"
@@ -115,23 +134,5 @@ def vehicles_available(est_household):
     labels = columns_to_labels(est_household, col_prefix=prefix, scrub=prefix)
     labels = labels.replace({"no" : "none"})
     labels.name = "vehicles_available"
-
-    return labels
-
-
-def household_type(est_household):
-    prefix = "^hht_"
-    scrub = "hht_|_hhsize_|_2p|_3p|_4p|_5p|_6p|_7pm|"
-    labels = columns_to_labels(est_household, col_prefix=prefix, scrub=scrub)
-    labels.name = "household_type",
-
-    return labels
-
-
-def household_size(est_household):
-    prefix = "^hht_fam_|^hht_nonfam_"
-    scrub = prefix + "|_hhsize_"
-    labels = columns_to_labels(est_household, col_prefix=prefix, scrub=scrub)
-    labels.name = "household_size"
 
     return labels
